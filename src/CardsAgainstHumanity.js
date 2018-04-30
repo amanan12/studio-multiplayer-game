d2862e70b24914a703dd99b96459409f5bfd567a
Abdul Manan <amanan@aoiths.org>
2018-04-30 22:26:46 +0000

    Progress

--- src/CardsAgainstHumanity.js
+++ src/CardsAgainstHumanity.js
@@ -4,10 +4,13 @@ import React, { Component } from 'react';
 import firebase from 'firebase';
 import Paper from 'material-ui/Paper';
 
-const sentences = ["1. ________.", "2. ________?", "3. ________?", "4. ________?", "5. ________?", "6. ________?", "7. ________?", "8. ________?", "9. ________?", "10. ________?"];
+const sentences = ["Whats that sound?", "I'm sorry Professor. I couldn't complete my homework because of ______.", "What is batman guilty pleasure?", "Whats my secret power?", "What gets better with age?", "What never fails to liven up the party?", "BILLY MAYS HERE FOR ______!", "Why can't I sleep at night?", "Whats the next Happy Meal Toy?", "The meaning of life."];
 const a = Math.floor(Math.random() * sentences.length);
+const answers = ["Kanye West.", "President Donald Trump.", "Science.", "Religion.", "Scientology.", "Judge Judy.", "The Pope.", "The United States Airforce.", "John Cena.", "Peace on Earth", "Dead memes.", "Barney.", "New York City Transit", "The Internet.", "Elon Musk", "Crash Royale", "Dwayne 'The Rock' Johnson.", "Tim Cooks." ];
 
+var random;
 
+var currentUser;
 
 const style = {
   margin: 12,
@@ -22,112 +25,93 @@ export default class CardsAgainstHumanity extends Component {
     constructor(props){
         super(props);
         this.state = {
-          label: "",
           choice: "",
-          opponent: ""
+          opponent: "",
+          user1: "",
+          user2: ""
         };
     }
     
     componentWillMount() {
-      // var id = this.props.match.params.id;
-      // var sessionDatabaseRef = firebase.database().ref("/session/" + id);
-      // console.log(sessionDatabaseRef);
-      // var sessionRef = firebase.database().ref(sessionDatabaseRef);
-      // console.log(sessionRef);
-      // var avatarRef = sessionRef.child("Pick");
-      // console.log(avatarRef.path.pieces);
-      // var currentUser = firebase.auth().currentUser.uid;
-      // var valu = (sessionDatabaseRef + "/" + currentUser);
-      
-      // avatarRef.on("value", function(snapshot) {
-      //   var a = snapshot.val();
-      //   console.log(a.Pick);
-      // });
-
-      
-      // console.log( "ID: " + currentUser );
-      // var a = this.state.label;
-      
       
       var id = this.props.match.params.id;
-      var sessionDatabaseRef = firebase.database().ref("/session/");
+      var sessionDatabaseRef = firebase.database().ref("/session/" + id);
+      
+      currentUser = firebase.auth().currentUser.uid;
+      
+      console.log("ID: " + currentUser);
+      
       var sessionRef = firebase.database().ref(sessionDatabaseRef);
+      console.log(sessionRef);
       
-      var currentUser = firebase.auth().currentUser.uid;
+      var valu = (sessionDatabaseRef + "/" + currentUser);
       
-      // console.log(id);
-      // console.log(currentUser);
-      
-      var avatarRef = sessionDatabaseRef.child(id);
-      
-      avatarRef.on("value", function(snapshot) {
-        var currentUser = firebase.auth().currentUser.uid;
-        var avatar = snapshot.val();
-        console.log(avatar.currentUser);
-      });
-
-      
-      // console.log(id);
-      
-      // console.log(sessionDatabaseRef);
-      
-      
-      const answers = ["a", "b", "c", "d", "e"];
-      const som = Math.floor(Math.random() * answers.length);
-      
-    }
-    
-    answer(){
-      const answers = ["a", "b", "c", "d", "e"];
-      const som = Math.floor(Math.random() * answers.length);
-      return answers[som];
     }
     
     clicker(b){
       
-      this.setState({
-        label: b
-      });
+      if(currentUser === "u9NXXkdWkRNUqtnk200csqurSB93"){
+        console.log(true);
+        this.setState({
+          choice: b
+        });
+      } else if(currentUser === "Kw5pj7nHWBQ6DalhqF0TPXvi0s13") {
+        console.log(false);
+        this.setState({
+          opponent: b
+        });
+      }
       
-      console.log(b);
       
-      this.avatarRef.update({ Pick: b });
+      // u9NXXkdWkRNUqtnk200csqurSB93
+      // Kw5pj7nHWBQ6DalhqF0TPXvi0s13
+    }
+    
+    checker(){
       
-      // var userChoice = {};
-      // userChoice[firebase.auth().currentUser.uid] = b;
-      // var sessionId = this.props.match.params.id;
-      // var sessionDatabaseRef = firebase.database().ref("/session/" + sessionId);
-      // sessionDatabaseRef.set(userChoice, (error) => {
-      //   if (error) {
-      //     console.error("Error storing session metadata", error);
-      //   }
-      // });
-      // this.setState({choice: b});
-      
-      // console.log(this.state.choice);
+      if(currentUser === "u9NXXkdWkRNUqtnk200csqurSB93"){
+        this.setState({
+          user1: 0
+        });
+      } else if(currentUser === "Kw5pj7nHWBQ6DalhqF0TPXvi0s13"){
+        this.setState({
+          user2: 1
+        });
+      }
       
     }
     
+    
+    
     render() {
+      
+      console.log(this.state.choice);
+      console.log(this.state.opponent);
+      console.log(this.state.user1);
+      console.log(this.state.user2);
+      
       return (<div>
         
         <div>
           
           <div className="cardShow">
-            <Paper className="aB" style={style} zDepth={3}>{this.state.label}</Paper>
+            <Paper className="ab" style={style} zDepth={3}>{this.state.choice}</Paper>
+            <Paper className="ba" style={style} zDepth={3}>{this.state.opponent}</Paper>
           </div> 
           
-          <div className="card">{sentences[a] + ", " + (a+1)}</div>
+          <div className="card">{sentences[a]}</div>
           
         </div>
         
+        <button className="btn" onClick={ () => this.checker() } >Confirm Selection</button>
+        
         <div className="cards">
         
-          <Paper className="a" onClick={ () => this.clicker("First Card") } style={style} zDepth={3} >{this.answer()}</Paper>
-          <Paper className="b" onClick={ () => this.clicker("Second Card") } style={style} zDepth={3} >{this.answer()}</Paper>
-          <Paper className="c" onClick={ () => this.clicker("Third Card") } style={style} zDepth={3} >{this.answer()}</Paper>
-          <Paper className="d" onClick={ () => this.clicker("Forth Card") } style={style} zDepth={3} >{this.answer()}</Paper>
-          <Paper className="e" onClick={ () => this.clicker("Fifth Card") } style={style} zDepth={3} >{this.answer()}</Paper>
+          <Paper className="a" onClick={ () => this.clicker( answers[4]  ) } style={style} zDepth={3} >{answers[4]}</Paper>
+          <Paper className="b" onClick={ () => this.clicker( answers[6] ) } style={style} zDepth={3} >{answers[6]}</Paper>
+          <Paper className="c" onClick={ () => this.clicker( answers[8] ) } style={style} zDepth={3} >{answers[8]}</Paper>
+          <Paper className="d" onClick={ () => this.clicker( answers[3] ) } style={style} zDepth={3} >{answers[3]}</Paper>
+          <Paper className="e" onClick={ () => this.clicker( answers[7] ) } style={style} zDepth={3} >{answers[7]}</Paper>
           
         </div> 
         
